a24b71f0f996e44af16686482fedb47a6fabfb28
Abhinandan <abhinandanshah042@gmail.com>
2025-07-25 12:55:17 +0530

    add multer and cloudinary for file upload

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,16 +1,23 @@
 class ApiError extends Error {
-  constructor(message = "Something went wrong", statusCode , error = [], statck = "") {
-    super(message);
-    this.statusCode = statusCode;
-    this.data = null
-    this.message = message;
-    this.success = false;
-    this.error = error; // Store additional error information
-    if(statck) {
-      this.stack = statck; // Store stack trace if provided
-    }else{
-        Error.captureStackTrace(this, this.constructor); // Capture stack trace
+    constructor(
+        statusCode,
+        message = "Something went wrong",
+        errors = [],
+        stack = ""
+    ) {
+        super(message)
+        this.statusCode = statusCode,
+        this.data = null,
+        this.errors = errors,
+        this.message = message,
+        this.success = false
+
+        if (stack) {
+            this.stack = stack
+        } else {
+            Error.captureStackTrace(this, this.constructor)
+        }
     }
 }
-}
-export default ApiError;
+
+export { ApiError }
--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,8 +1,10 @@
-class ApiResponse{
-    constructor(statusCode , message = "Success") {
-        this.statusCode = statusCode;
+class ApiResponse {
+    constructor(statusCode, data, message = "success") {
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
         this.success = statusCode < 400
-        this.message = message;
-        this.data = data // Initialize data to null
     }
 }
+
+export { ApiResponse }
